f7899e7e30f99b08bcdd1fc9684aefc3f9a19c73
Davidson Simões <davidsonsimoes@MacBook-Pro-de-Davidson.local>
2018-07-19 00:56:38 -0300

    iniciando login

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { Row, Col, Form, FormGroup, FormControl, ControlLabel, Button, Panel } from 'react-bootstrap';
+import gql from 'graphql-tag';
+// import swal from 'sweetalert';
+import client from '../services/Apollo'
 
 export default class Login extends React.Component  {
+    state = { 
+        email: "davidson.simoes22@gmail.com"
+    };
+  async checkUser(e) {
+      e.preventDefault();
+        let data = [];
+        await client.query({
+            query: gql`
+            {
+                Profile(email: "${this.state.email}"){
+                  name,
+                  email,
+                  password
+                }
+              }
+            `
+            })
+        .then(result => data = result);
+        console.log(data)
+  }
   render() {
     return (
       <div className="App">
@@ -30,7 +53,7 @@ export default class Login extends React.Component  {
                 <Row>
                 <Col sm={12}>
                     <FormGroup>                    
-                    <Button type="submit">Entrar</Button>
+                    <Button type="submit" onClick={(e) => this.checkUser(e)}>Entrar</Button>
                     </FormGroup>
                 </Col>
                 </Row>
